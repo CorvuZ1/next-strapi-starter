6209b5df080cc0850551cceeaa4e1322144ea725
CorvuZ1 <igor@DESKTOP-9CM1SN4>
2023-09-07 22:24:46 +0300

    add nginx

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,20 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "connect-src": ["'self'", "http:", "https:"]
+        }
+      }
+    }
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public"
 ];
