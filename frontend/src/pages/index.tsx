55779d3fa37e3bd7e208f7de22dc590425d4e912
CorvuZ1 <igor@DESKTOP-9CM1SN4>
2023-09-09 22:25:11 +0300

    change api url

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
-    <div className="flex justify-center bg-gradient-to-r from-cyan-500 to-blue-500 py-5 text-6xl font-bold">
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 py-5 text-center text-6xl font-bold text-white">
       LET'S GO
     </div>
   );
